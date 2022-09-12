54ba9d4c4e1a73a4ab00543d0d7d11d91ade94fe
RasmusUni <202004598@post.au.dk>
2022-09-12 19:29:26 +0200

    first attempt

--- src/lis.py
+++ src/lis.py
@@ -4,6 +4,7 @@
 # They *should* be comparable, and it *is* possible to make such
 # an annotation, but it is tricky, and I don't want to confuse you
 # more than strictly necessary.
+from pickle import TRUE
 from typing import Sequence, Any
 
 
@@ -46,7 +47,23 @@ def longest_increasing_substring(x: Sequence[Any]) -> tuple[int, int]:
     >>> longest_increasing_substring([12, 45, 32, 65, 78, 23, 35, 45, 57])
     (5, 9)
     """
-    # The leftmost empty string is our first best bet
-    best = (0, 0)
+    for i in range(len(x)+1):
+        list1=[]
+        y=0
+        if is_increasing([x[i],x[i+1]])==True:
+            list1.append(i)
+            a=i
+            b=i+1
+            while is_increasing([x[i+a],x[i+b]])==True:
+                a+=1
+                b+=1
+            list1.append(a)
+            if substring_length(list1)>y:
+                y=substring_length(list1)
+                best=(list1[0],list1[1])
+
+
     # FIXME: explore the other possibilities
     return best
+
+
